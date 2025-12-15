c9af7386fd908602ab47fabc753cd1f54e6df359
AnChekhov <andrey.xpost@gmail.com>
2025-12-15 22:59:56 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,10 @@ export default {
     extend: {
       colors: {
         tpp: {
-          primary: '#D8FB53',   // Светлый Лайм (Акцент)
-          primaryHover: '#BCE63B',
+          // МЕНЯЕМ ЛАЙМ НА ЗОЛОТОЙ (Yellow-500 из Tailwind)
+          primary: '#EAB308',   
+          primaryHover: '#CA8A04', // Yellow-600 для ховера
+          
           dark: '#0F172A',      // Slate 900 (Основа)
           secondary: '#06B6D4', // Electric Cyan (Поддержка)
           surface: '#F8FAFC',   // Slate 50 (Фон)
