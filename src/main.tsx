33e664c64f36d69822183ad5c473191b7957ee20
AnChekhov <andrey.xpost@gmail.com>
2025-12-18 23:02:13 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-// ✅ 1. Импортируем провайдер
+// ✅ ИЗМЕНЕНО: Импортируем HashRouter вместо BrowserRouter
+import { HashRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* ✅ 2. Оборачиваем App в HelmetProvider */}
     <HelmetProvider>
-      <BrowserRouter>
+      {/* ✅ ИЗМЕНЕНО: Используем HashRouter */}
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </React.StrictMode>,
 )
